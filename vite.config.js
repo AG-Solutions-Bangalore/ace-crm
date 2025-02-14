d370d28e3aae74ce77cf6b78c1c7cf48534e18c8
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-02-14 10:27:38 +0530

    `test the cross origin`

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://exportbiz.in',
+        target: 'ace-crm.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
